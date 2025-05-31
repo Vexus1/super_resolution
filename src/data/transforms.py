2560cfaeb3150e8f1c8728b3f31f5ab90a137a81
vexus1 <adriangalik123@gmail.com>
2025-06-01 00:47:03 +0200

    Add function to generate random HR-LR patch pairs

--- src/data/transforms.py
+++ src/data/transforms.py
@@ -14,9 +14,18 @@ def gaussian_blur(img: tf.Tensor, kernel: tf.Tensor) -> tf.Tensor:
     # blur_img = tf.cast(blur_img, tf.uint8)
     return blur_img[0]
 
-def synthesize_lr(hr_image: tf.Tensor, scale: int, kernel: tf.Tensor) -> tf.Tensor:
+def synthesize_lr(hr_image: tf.Tensor, 
+                  scale: int, kernel: tf.Tensor) -> tf.Tensor:
     hr_image = gaussian_blur(hr_image, kernel)
     h, w = tf.shape(hr_image)[0], tf.shape(hr_image)[1]
     lr_hw = (h // scale, w // scale)
     lr_image = tf.image.resize(hr_image, lr_hw, method="bicubic")
     return tf.image.resize(lr_image, (h, w), "bicubic")
+
+def random_patch_pair(hr_image: tf.Tensor, scale: int, fsub: int,
+                      kernel: tf.Tensor) -> tuple[tf.Tensor, tf.Tensor]:
+    top = tf.random.uniform([], 0, tf.shape(hr_image)[0] - fsub + 1, tf.int32)
+    left = tf.random.uniform([], 0, tf.shape(hr_image)[1] - fsub + 1, tf.int32)
+    hr_patch = tf.image.crop_to_bounding_box(hr_image, top, left, fsub, fsub)
+    lr_patch = synthesize_lr(hr_patch, scale, kernel)
+    return lr_patch, hr_patch
